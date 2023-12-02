28092c0f1d76ba3f6ec64009204239dfb43dc87b
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2023-12-02 23:27:49 +0100

    day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "github.com/Zouheir-Barhoumi/advent/day2/part1"
+import (
+	"github.com/Zouheir-Barhoumi/advent/day2/part2"
+)
 
 func main() {
-	part1.Part1()
+	// part1.Part1()
+	part2.Part2()
 }
---
+++ day2/part2/partTwo.go
@@ -0,0 +1,171 @@
+package part2
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func checkDigit(d byte) bool {
+	// unicode.IsDigit takes rune only
+	return unicode.IsDigit(rune(d))
+}
+func Part2() {
+	/**
+	**Determine which games would have been possible if the bag had been loaded with only 12 red cubes,
+	** 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+	Scan input file line by line
+	*/
+
+	colors := make(map[string]int)
+
+	colors["red"] = 0
+	colors["green"] = 0
+	colors["blue"] = 0
+
+	games := make([][]string, 0, 128)
+
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scn := bufio.NewScanner(file)
+
+	for scn.Scan() {
+
+		scannedLine := scn.Text()
+		titleIndex := strings.Index(scannedLine, ":") + 1
+		filtered := scannedLine[titleIndex:]
+
+		trimmed := strings.Trim(filtered, " ")
+
+		tokenized := strings.Split(trimmed, ";")
+
+		games = append(games, tokenized)
+
+		// fmt.Println(len(scn.Bytes()))
+	}
+
+	sum := 0
+	possible := true
+	powerOfMinSet := 0
+	sumOfPowers := 0
+	for gId, game := range games {
+		fmt.Println(game)
+
+		var minRed, minGreen, minBlue int = 0, 0, 0
+		for setId := 0; setId < len(game); setId++ {
+			set := game[setId]
+			fmt.Println(set)
+
+			for i := 0; i+3 < len(set); i++ {
+				if checkDigit(set[i]) {
+					if checkDigit(set[i+1]) {
+						// We have a two-digit number
+						doubleDigit := string(set[i]) + string(set[i+1])
+
+						color := set[i+3]
+						fmt.Printf("The Color Initial is %c\n", color)
+
+						// INCREMENT COLORs FREQUENCIES
+						switch string(color) {
+						case "r":
+							redDigit, _ := strconv.Atoi(doubleDigit)
+							colors["red"] += redDigit
+							if minRed < redDigit {
+								minRed = redDigit
+							}
+						case "g":
+							greenDigit, _ := strconv.Atoi(doubleDigit)
+							colors["green"] += greenDigit
+							if minGreen < greenDigit {
+								minGreen = greenDigit
+							}
+						case "b":
+							blueDigit, _ := strconv.Atoi(doubleDigit)
+							colors["blue"] += blueDigit
+							if minBlue < blueDigit {
+								minBlue = blueDigit
+							}
+						}
+						i++
+						continue
+					} else {
+						// we have a one-digit number
+						singleDigit := string(set[i])
+						color := set[i+2]
+						fmt.Printf("The Color Initial is %c\n", color)
+
+						switch string(color) {
+						case "r":
+							redDigit, _ := strconv.Atoi(singleDigit)
+							colors["red"] += redDigit
+							if minRed < redDigit {
+								minRed = redDigit
+							}
+						case "g":
+							greenDigit, _ := strconv.Atoi(singleDigit)
+							colors["green"] += greenDigit
+							if minGreen < greenDigit {
+								minGreen = greenDigit
+							}
+						case "b":
+							blueDigit, _ := strconv.Atoi(singleDigit)
+							colors["blue"] += blueDigit
+							if minBlue < blueDigit {
+								minBlue = blueDigit
+							}
+						}
+
+						// INCREMENT COLORs FREQUENCIES
+					}
+				}
+
+			}
+
+			fmt.Println("---------------------------------")
+
+			fmt.Printf("RED: %d \t GREEN: %d \t BLUE: %d \t \n", colors["red"], colors["green"], colors["blue"])
+			if colors["red"] > 12 || colors["green"] > 13 || colors["blue"] > 14 {
+				// fmt.Printf("\n\n\n\n\nThis line %d is POSSIBLE!\n\n\n\n", gId)
+				possible = false
+			}
+			fmt.Printf("----------End Of Set-------------\n\n")
+			// reset colors with each set
+			colors["red"] = 0
+			colors["green"] = 0
+			colors["blue"] = 0
+		}
+		powerOfMinSet = minRed * minGreen * minBlue
+
+		fmt.Printf("MIN_RED: %d \t MIN_GREEN: %d \t MIN_BLUE: %d \n ", minRed, minGreen, minBlue)
+		fmt.Println("-----------------------")
+		fmt.Println(" \t Power of the minimum set of cubes \t  \n ", powerOfMinSet)
+		fmt.Println("-----------------------")
+		fmt.Printf("RED: %d \t GREEN: %d \t BLUE: %d \n ", colors["red"], colors["green"], colors["blue"])
+
+		colors["red"] = 0
+		colors["green"] = 0
+		colors["blue"] = 0
+		fmt.Println("============================")
+		if possible {
+			fmt.Printf("\n\n\nThis line %d is possible\n\n\n", gId+1)
+			sum += gId + 1
+
+		} else {
+			fmt.Printf("\n\n\nThis line %d is IMPOSSIBLE\n\n\n", gId+1)
+		}
+		fmt.Printf("\n\n\nTotal Possible: %d\n\n\n", sum)
+		sumOfPowers += powerOfMinSet
+		fmt.Printf("\n\n\nTotal Sum of Powers: %d\n\n\n", sumOfPowers)
+		possible = true
+	}
+
+}
